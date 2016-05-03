4a9627e55e935dbdec0a798a08197290e5e85edb
Igor Klekotnev <klekotnev@gmail.com>
2016-05-03 01:28:53 -0700

    devise ...

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,10 +16,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'klekotnev@gmail.com'
-<<<<<<< HEAD
 
-=======
->>>>>>> develop
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
