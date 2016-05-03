9c53c911f28b01b009d4cbe928102661d8f40260
Igor Klekotnev <klekotnev@gmail.com>
2016-05-03 11:50:08 -0700

    edit registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,10 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    '/' # Or :prefix_to_your_route
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'search', to: 'search#search'
 
   #get 'registrations/sign_up_params'
   #get 'registrations/account_update_params'
-  root 'home#index'
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   resources :articles
